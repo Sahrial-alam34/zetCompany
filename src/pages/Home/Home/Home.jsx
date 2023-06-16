4b5c0d1d8283e587bf9113b07c9cd9e7c0459f09
Sahrial-alam34 <sunnykcp01@gmail.com>
2023-06-16 18:58:47 +0600

    add navbar

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../Navbr/Navbar";
+
+
+const Home = () => {
+    return (
+        <div>
+           <Navbar></Navbar>
+           <hr />
+        </div>
+    );
+};
+
+export default Home;
